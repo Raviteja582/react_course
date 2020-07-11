a38a9dd6a7f8c6d14f3a82177c4b553ad7a80a12
Raviteja582 <raviteje1028312@gmail.com>
2020-07-11 11:07:42 +0530

    Creating a Component for the Images.
    1.We create a component folder in "src"
    2.We create a menucom.js file where we creates a class Menu which returns the Media object of all dishes.
    3.Then we export this component to App.js file by "import Menu from './components/menucom'.
    4.Commented out the default App.css file.
    5.Removed className="App" from App.js div tag.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
+import Menu from './components/menucom';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
 			<NavbarBrand href="/">My Restraunt</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
