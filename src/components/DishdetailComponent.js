3e5307b4f1a14f48f89a54659fd4c1af98f05afe
Raviteja582 <raviteje1028312@gmail.com>
2020-07-14 12:56:07 +0530

    MainComponent.js is implemented for presentational Component Rather than App.js
    1.We Created Seperate components MainComponent.js for displaying the content and sharing resourse to the Child components.
    2.We just make the container component by implementing the Main component and Main component as parent component
    for MenuComponent and DishdetailComponent
    3.DishDetailComponent is responsible for giving comments and dish card.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,22 +1,45 @@
 import React,{Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, CardText, CardBody } from 'reactstrap';
-class Details extends Component{
+class Details extends React.Component{
     constructor(props){
         super(props);
     }
     render(){
-        return (
-            <Card key={this.props.dish.id}>
-                <CardImg width="200%" height="200%" src={this.props.dish.image} alt={this.props.dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{this.props.dish.name}</CardTitle>
-                </CardImgOverlay>
-                <CardBody>
-                    <CardTitle>{this.props.dish.name}</CardTitle>
-                    <CardText>{this.props.dish.description}</CardText>
-                </CardBody>
-            </Card>
-        );
+        const d=this.props.dish;
+        if(d!=null){
+            return  (
+                <div className="container">
+                    <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                    <Card>
+                       <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                           <CardBody>
+                           <CardTitle>{this.props.dish.name}</CardTitle>
+                           <CardText>{this.props.dish.description}</CardText>
+                               </CardBody>
+                       </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                      <b>Comments</b>
+                        {this.props.dish.comments.map( (comment) => {
+                            return(
+                                <div>
+                                    <p>{comment.comment}</p>
+                                    <p>--{comment.author} , {comment.date}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    </div>
+                </div>
+                       
+            );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
     }
 }
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,91 +1,28 @@
 import React, { Component } from 'react';
-import {Media, ToastHeader} from 'reactstrap';
-import { ListGroup, ListGroupItem } from 'reactstrap';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+import { Card, CardImg, CardImgOverlay,
     CardTitle } from 'reactstrap';
-import Details from './DishdetailComponent';
+
 class Menu extends Component{
     constructor(props) {
         super(props);
-        this.state = {
-            selectedDish: null
-        }
-        console.log('Menu Components constructor is Called')
-    }
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
-    }
-    renderComment(dish){
-        if(dish!=null){
-            const com=dish.comments.map((c) =>{
-                return(
-                    <div>
-                        <li>
-                        {c.comment}<br></br>
-                        --{c.author}, {c.date}
-                        </li>
-                        <br></br>
-                    </div>
-                );    
-            });
-            return(
-                <div>
-                    <h4>Comments</h4><br></br>
-                    <ul className="list-unstyled">
-                        {com}
-                    </ul>
-                </div>
-                
-            );
-        }
-        else{
-            return(
-                <div>
-                </div>
-            );
-        }
-    }
-    renderDish(dish) {
-        console.log("Render dish is called");
-        if (dish != null)
-            return(
-                <Details dish={this.state.selectedDish} />
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
-    componentDidMount(){
-        console.log('Menu Components ComponentDid Mount is Called')
     }
     render(){
-        const menu = this.props.dishes.map((dish) => {
+        const menu = this.props.dish.map((dis) => {
             return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
+              <div key={dis.id} className="col-12 col-md-5 m-1">
+                <Card onClick={() => this.props.onClick(dis.id)}>
+                  <CardImg width="100%" src={dis.image} alt={dis.name} />
                   <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
+                      <CardTitle>{dis.name}</CardTitle>
                   </CardImgOverlay>
                 </Card>
               </div>
             );
         });
-        console.log('Menu Components Render is Called')
         return (
             <div className="container">
                 <div className="row">
                     {menu}
-                </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 ml-1">
-                        {this.renderDish(this.state.selectedDish)}
-                    </div>
-                    <div className="col-12 col-md-5 mr-1">
-                        {this.renderComment(this.state.selectedDish)}
-                    </div>
                 </div>
             </div>
         );
