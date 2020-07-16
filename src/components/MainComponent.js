c16838195d5611c49cc8accd2f1deca462136e63
Raviteja582 <raviteje1028312@gmail.com>
2020-07-16 15:13:56 +0530

    Added AboutComponent in the MainComponent.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import {DISHES} from '../shared/dishes';
 import Details from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch,Route,Redirect } from 'react-router-dom';
@@ -53,6 +54,7 @@ class Main extends Component {
 					<Route exact path="/menu" component={() => <Menu dish={this.state.dishes} />} />
 					<Route path='/menu/:dishId' component={DishWithId} />
 					<Route exact path="/contactus" component={Contact} />
+					<Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
 					<Redirect to="/home" />
 				</Switch>
 				<Footer />
